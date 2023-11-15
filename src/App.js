a95ff54b7a9150d9c9879972fe568084b48b1c05
jam860 <jam860@uw.edu>
2023-11-15 11:35:48 -0800

    made data into json

--- src/App.js
+++ src/App.js
@@ -8,55 +8,12 @@ import { Itinerary } from './Itinerary';
 import { User } from './User';
 import { EventForm } from './EventForm.js';
 import { Event } from './Event.js';
+import SAMPLE_TRIPS from "./data/featuredData.json";
+import USER_TRIPS from "./data/userData.json";
 
 function App() {
-  //deleting this soon, dont use this
-  const itinerariesData = [
-    {title: "Dazzling Kyoto", firstStop: "Nijo Castle", image: "/img/kyoto-3.jpg"},
-    {title: "GirlsinCanada", firstStop: "Green Clover Canada", image: "/img/canada.jpeg"},
-    {title: "Wine in Italy", firstStop: "Frutti & Gelato", image: "/img/italy.jpeg"},
-    {title: "Clubbing in Korea", firstStop: "The Weekend Hongdae Club", image: "/img/korea.jpg"}
-  ];
-
-  //use this data
-  let tripsData = [
-    {
-        tripName: "Dazzling Kyoto", startDate: "2024-06-01", endDate: "2024-06-02", destination: "Japan", notes: "", photo: "/img/kyoto-3.jpg",
-        events: [{ eventName: "Leave Kyoto", date: "2024-06-02", startTime: "08:00", endTime: "10:00", address: "541 Nijōjōchō, Nakagyo Ward, Kyoto, 604-8301, Japan", eventType: "Activity", img: "/img/airport-kyoto.jpg", notes: "" },
-        { eventName: "Land in Kyoto", date: "2024-06-01", startTime: "08:00", endTime: "10:00", address: "1 Senshukukokita, Izumisano, Osaka 549-0001, Japan", eventType: "Activity", img: "/img/airport-kyoto.jpg", notes: "" },
-        { eventName: "Nijo Castle", date: "2024-06-01", startTime: "10:00", endTime: "11:00", address: "541 Nijōjōchō, Nakagyo Ward, Kyoto, 604-8301, Japan", eventType: "Activity", img: "/img/nijo-castle.jpg", notes: "" },
-        { eventName: "Lunch", date: "2024-06-01", startTime: "12:00", endTime: "14:00", address: "902 Higashishiokojicho, Shimogyo Ward, 600-8216, Japan", eventType: "Activity", img: "/img/ramen.jpg", notes: "" },
-        { eventName: "Dinner", date: "2024-06-01", startTime: "18:00", endTime: "20:00", address: "541 Nijōjōchō, Nakagyo Ward, Kyoto, 604-8301, Japan", eventType: "Activity", img: "/img/ramen.jpg", notes: "" },
-        { eventName: "Eat at Kyoto Airport", date: "2024-06-02", startTime: "07:00", endTime: "08:00", address: "541 Nijōjōchō, Nakagyo Ward, Kyoto, 604-8301, Japan", eventType: "Activity", img: "/img/airport-kyoto.jpg", notes: "" }]
-    },
-    { tripName: "GirlsinCanada",
-      startDate: "2024-07-01",
-      endDate: "2024-07-02",
-      destination: "Canada",
-      notes: "",
-      photo: "/img/canada.jpeg",
-      events: []
-    },
-    {
-      tripName: "Wine in Italy", 
-      startDate: "2024-08-01",
-      endDate: "2024-08-02",
-      destination: "Italy", 
-      notes: "",
-      photo: "/img/italy.jpeg",
-      events: []
-    },
-    { tripName: "Clubbing in Korea",
-      startDate: "2024-09-01",
-      endDate: "2024-09-02",
-      destination: "South Korea",
-      notes: "",
-      photo: "/img/korea.jpg",
-      events: []
-    }
-
-    //create more external data using chatgpt potentially
-  ];
+  let sampleData = SAMPLE_TRIPS; //use featuredData.json for home page;
+  let tripsData = USER_TRIPS; //for itinerary page
 
   function addTrip(trip) {
     tripsData.unshift(trip);
