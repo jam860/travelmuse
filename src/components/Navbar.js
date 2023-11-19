a78bc47839e0dd02e61ea42634240d714259bffc
SirBlank <amberyuewu@gmail.com>
2023-11-19 03:21:04 -0800

    added hamburger nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { NavLink } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
 
 export function Navbar() {
     return (
@@ -8,7 +9,7 @@ export function Navbar() {
                     <div className="logo" role="button">
                         <NavLink to="/"><img width="300" height="50" src="/img/logo.png" aria-label="TravelMuse logo" alt="TravelMuse logo" /></NavLink>
                     </div>
-                    <div className="nav-links">
+                    <div className="nav-links desktop-nav">
                         <ul>
                             <li>
                                 <NavLink to="/">Home</NavLink>
@@ -24,6 +25,20 @@ export function Navbar() {
                             </li>
                         </ul>
                     </div>
+                    <nav className="hamburger-nav">
+                        <Dropdown className="hamburger-menu">
+                            <Dropdown.Toggle menuVariant="#FAB573">
+                                <span className="material-icons icon-center">&#xE5D2;</span>
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item as={NavLink} to="/">Home</Dropdown.Item>
+                                <Dropdown.Item as={NavLink} to="plan">Plan</Dropdown.Item>
+                                <Dropdown.Item as={NavLink} to="mytrips">Trips</Dropdown.Item>
+                                <Dropdown.Item as={NavLink} to="#">Sign-in</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </nav>
                 </div>
             </nav>
         </header>
